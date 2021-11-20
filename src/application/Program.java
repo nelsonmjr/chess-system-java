9d4db0942152653df8b720f4e38b40e75183564a
nelsonmjr <nelsonmjr@gmail.com>
2021-11-20 15:29:47 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,5 +7,11 @@ public class Program {
 		
 		//Moving pieces
 	}
+	
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+	System.out.print("\033[H\033[2J");
+	System.out.flush();
+	}
 
 }
